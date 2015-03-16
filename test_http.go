958fdee536f0f68dc01197d64c3de3258222ae4d
管宜尧 <mylxsw@126.com>
2015-03-16 10:34:50 +0800

    修正test_http.go 写入文件失败，增加gitignore文件

--- test_http.go
+++ test_http.go
@@ -10,13 +10,16 @@ import (
 	"strconv"
 )
 
+// 礼包内容结构体
 type Libao struct {
-	title string
-	code  string
-	fsize string
+	title string // 标题
+	code  string // 礼包码
+	fsize string // 文件大小
 }
 
+// 抓取指定页面的所有礼包
 func FetchLibaos(page int) (libaos []Libao) {
+	// 请求页面信息
 	url := "http://www.libao.so/index.php?page=" + strconv.Itoa(page)
 	log.Println(url)
 	resp, err := http.Get(url)
@@ -34,27 +37,32 @@ func FetchLibaos(page int) (libaos []Libao) {
 	body_html := string(body[:len(body)])
 	log.Println(body_html)
 
+	// 初始化匹配正则表达式
 	re := regexp.MustCompile("<tr>([\\s\\S]*?)</tr>")
 
 	re_title := regexp.MustCompile("<a target=\"_blank\" href=\"(.*?)\">(.*?)</a>")
 	re_fsize := regexp.MustCompile("<td style=\"text-align:center;\">(.*?)</td>")
 	re_code := regexp.MustCompile("<td style=\"color:red;\">(.*?)</td>")
 
+	// 匹配内容
 	tag_trs := re.FindAllString(body_html, -1)
 	for i := 0; i < len(tag_trs); i++ {
+		// 礼包标题
 		tag_title := re_title.FindStringSubmatch(tag_trs[i])
 
 		if len(tag_title) > 2 && tag_title[2] != "" {
+			// 礼包码
 			tag_code := re_code.FindStringSubmatch(tag_trs[i])
 
 			if len(tag_code) > 1 && tag_code[1] != "" {
-
+				// 礼包大小，如果没有查询到，则使用 unknown
 				fsize := "unknown"
 				tag_fsize := re_fsize.FindStringSubmatch(tag_trs[i])
 				if len(tag_fsize) > 1 && tag_fsize[1] != "" {
 					fsize = tag_fsize[1]
 				}
 
+				// 追加到返回结果
 				libaos = append(libaos, Libao{
 					title: tag_title[2],
 					code:  tag_code[1],
@@ -66,8 +74,9 @@ func FetchLibaos(page int) (libaos []Libao) {
 	return
 }
 
+// 将礼包信息写入到文件
 func WriteHtml(filename string, libaos []Libao) {
-	file, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND, 0644)
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -75,7 +84,10 @@ func WriteHtml(filename string, libaos []Libao) {
 
 	for i := 0; i < len(libaos); i++ {
 		record := fmt.Sprintf("%-20s :::: %s :::: %s\n", libaos[i].title, libaos[i].code, libaos[i].fsize)
-		file.WriteString(record)
+		_, err := file.WriteString(record)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
